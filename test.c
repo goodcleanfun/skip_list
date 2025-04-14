3d629670cc253143fb92febfe0d1aed0f16be802
Al <albarrentine@gmail.com>
2025-04-13 20:38:10 -0400

    [test] adding concurrent tests for a lock-free skip list (well, mostly, memory pool has only a single mutex when resizing but it is only held once per block allocation

--- test.c
+++ test.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #include "greatest/greatest.h"
+#include "threading/threading.h"
 
 #define SKIP_LIST_NAME skip_list_uint32
 #define SKIP_LIST_KEY_TYPE uint32_t
@@ -55,6 +56,8 @@ TEST test_skip_list(void) {
     e = skip_list_uint32_get_prev(list, 10);
     ASSERT_STR_EQ("e", e);
 
+    ASSERT_EQ(skip_list_uint32_size(list), 6);
+
     a = skip_list_uint32_delete(list, 1);
     ASSERT_STR_EQ(a, "a");
   
@@ -64,8 +67,12 @@ TEST test_skip_list(void) {
     b = skip_list_uint32_delete(list, 3);
     ASSERT_STR_EQ(b, "b");
 
+    ASSERT_EQ(skip_list_uint32_size(list), 4);
+
     e = skip_list_uint32_delete(list, 9);
     ASSERT_STR_EQ(e, "e");
+
+    ASSERT_EQ(skip_list_uint32_size(list), 3);
 
     c = skip_list_uint32_get(list, 5);
     ASSERT_STR_EQ(c, "c");
@@ -80,6 +87,9 @@ TEST test_skip_list(void) {
     ASSERT(d == NULL);
 
     skip_list_uint32_insert(list, 7, "d");
+
+    ASSERT_EQ(skip_list_uint32_size(list), 2);
+
     d = skip_list_uint32_get(list, 7);
     ASSERT_STR_EQ(d, "d");
 
@@ -96,7 +106,62 @@ TEST test_skip_list(void) {
     PASS();
 }
 
+#define SKIP_LIST_NAME concurrent_skip_list_uint32
+#define SKIP_LIST_KEY_TYPE uint32_t
+#define SKIP_LIST_VALUE_TYPE char *
+#define SKIP_LIST_THREAD_SAFE
+#include "skip_list.h"
+#undef SKIP_LIST_NAME
+#undef SKIP_LIST_KEY_TYPE
+#undef SKIP_LIST_VALUE_TYPE
+#undef SKIP_LIST_THREAD_SAFE
 
+
+static char alphabet[26][2] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+struct thread_args {
+    concurrent_skip_list_uint32 *list;
+    uint32_t multiplier;
+};
+
+#define NUM_THREADS 8
+#define NUM_INSERTS 100000
+
+int test_skip_list_thread(void *arg) {
+    struct thread_args *args = (struct thread_args *)arg;
+    concurrent_skip_list_uint32 *list = args->list;
+    uint32_t multiplier = args->multiplier;
+    for (uint32_t i = multiplier; i < NUM_THREADS * NUM_INSERTS; i += NUM_THREADS) {
+        uint32_t key = i;
+        char *value = alphabet[i % 26];
+        concurrent_skip_list_uint32_insert(list, key, value);
+        char *fetched = concurrent_skip_list_uint32_get(list, key);
+        if (fetched == NULL || strcmp(value, fetched) != 0) {
+            fprintf(stderr, "i: %u, value: %s, expected: %s\n", key, value, fetched);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+TEST test_skip_list_multithreaded(void) {
+    concurrent_skip_list_uint32 *list = concurrent_skip_list_uint32_new();
+    struct thread_args args[NUM_THREADS];
+    thrd_t threads[NUM_THREADS];
+    for (uint32_t i = 0; i < NUM_THREADS; i++) {
+        args[i].list = list;
+        args[i].multiplier = i;
+        thrd_create(&threads[i], test_skip_list_thread, &args[i]);
+    }
+    for (uint32_t i = 0; i < NUM_THREADS; i++) {
+        thrd_join(threads[i], NULL);
+    }
+    size_t size = concurrent_skip_list_uint32_size(list);
+    ASSERT_EQ(size, NUM_THREADS * NUM_INSERTS);
+    concurrent_skip_list_uint32_destroy(list);
+    PASS();
+}
 
 /* Add definitions that need to be in the test runner's main file. */
 GREATEST_MAIN_DEFS();
@@ -104,8 +169,8 @@ GREATEST_MAIN_DEFS();
 int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();      /* command-line options, initialization. */
 
-    srand(0);
     RUN_TEST(test_skip_list);
+    RUN_TEST(test_skip_list_multithreaded);
 
     GREATEST_MAIN_END();        /* display results */
 }
